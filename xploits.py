4f7728b297d28d595cba6f801bf0d7c0619bd0a9
brijesh kumar <59088458+anonymousmani@users.noreply.github.com>
2020-04-04 19:21:06 +0530

    Update xploits.py

--- xploits.py
+++ xploits.py
@@ -4,7 +4,7 @@ import sys
 def banner():
  os.system("apt install toilet")
  os.system("clear")
- os.system("toilet -fmono12 -F gay X_PLOITS")
+ os.system("toilet -fmono12 -F gay XPLOITS")
  print("    \033[1;36;40m Code made by: \033[1;32;40m anonymousmani")
  print("    \033[1;36;40m Inspired by : \033[1;32;40m tuhin bose")
  print("    \033[1;36;40m Instagram id: \033[1;32;40m www.instagram.com/x_ploits")
