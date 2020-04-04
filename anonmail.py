409863e26c9aeed4c6daa765098001a376216467
brijesh kumar <59088458+anonymousmani@users.noreply.github.com>
2020-04-04 19:23:10 +0530

    Update anonmail.py

--- anonmail.py
+++ anonmail.py
@@ -3,7 +3,7 @@ import os
 
 def banner():
  os.system("clear")
- os.system("toilet -fmono12 -F border X_PLOITS")
+ os.system("toilet -fmono12 -F border XPLOITS")
  print("    \033[1;36;40m TOOL        : \033[1;32;40m Send Anonymous Mails To Your Victim ")
  print("    \033[1;36;40m Code made by: \033[1;32;40m anonymousmani")
  print("    \033[1;36;40m Inspired by : \033[1;32;40m tuhin bose")
